43a56ec5bf0e32805ee108485dbb99da67c5881d
Obisoft Dev <104738729+ObisoftDev@users.noreply.github.com>
2022-10-30 05:44:22 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','obisoftt').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KEGEJEYEJELEFEYEKEKECEYEJEGEHEREDELEDELE'))
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KHGHJHYHJHLHFHYHDHGHYHJHHHCHRHDHLHDHLH'))
